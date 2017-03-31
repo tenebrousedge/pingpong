657ee4cc2c174a4cb7e7131b4109c916db8690e5
Patrick Leahy <tenebrousedge@gmail.com>
2017-03-31 09:19:37 -0700

    fix reference errors and refactor to use obj.prototype

--- README.md
+++ README.md
@@ -8,16 +8,16 @@ This is a tiny application which seeks to demonstrate this week's concepts, name
 * The program will not run for non-numeric input
 * If a number is entered, Ping Pong will count up to that number in increments of one.
   - Example Input: 7
-  - Example Output: 0 1 2 3 4 5 6 7
+  - Example Output: 1 2 3 4 5 6 7
 * For output numbers that are divisible by three, Ping Pong will replace those numbers with the word "Ping"
   - Example Input: 7
-  - Example Output: 0 1 2 Ping 4 5 Ping 7
+  - Example Output: 1 2 Ping 4 5 Ping 7
 * For output numbers which are divisible by five, Ping Pong will replace those numbers with the word "Pong"
   - Example Input: 11
-  - Example Output: 0 1 2 3 4 Pong 6 7 8 9 Pong 11
+  - Example Output: 1 2 3 4 Pong 6 7 8 9 Pong 11
 * For output numbers which are divisible by both three and five, Ping Pong will replace those numbers with the word "PingPong"
   - Example Input: 16
-  - Example Output: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 PingPong
+  - Example Output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 PingPong
 
 ## Installation
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,32 @@
-function pingPong(input) {
-  let this.inputString = input;
-  let this.outputArray = [];
+function PingPong(input) {
+  this.input = input;
+  this.outputArray = [];
+};
 
-  let validateInput = function() {
+PingPong.prototype = {
+  validateInput : function() {
     return ! /[^\d]/.test(this.input);
-  }
-
-  let generate = function() {
-    for (let i = 0; i <= number; i++){
+  },
+  generate : function() {
+    for (let i = 1; i <= this.input; i++){
       let string = '';
       if (!(i % 3)) string += 'Ping';
       if (!(i % 5)) string += 'Pong';
       this.outputArray.push(string || i);
     }
-    return outputArray; //could have been interesting to return `this` and define a toString method
+    return this.outputArray; //could have been interesting to return `this` and define a toString method
   }
-} 
+};
 
 $(document).ready(function() {
   $('form').submit(function() {
     let ping = new PingPong($('input').val());
     if (ping.validateInput()) {
       $('#output ul').empty();
-      $.each(ping.generate(), function(value) {
+      $.each(ping.generate(), function(idx, value) {
         $('<li>').text(value).appendTo('#output ul');
       });
-    }
+    };
     return false
   });
 });
